33b0a58546f69cd8a0f71bb7bfc4aae1ce079640
Beth Secor <elizabeth.a.secor@gmail.com>
2016-04-09 16:34:16 -0600

    Finish creact tutorial

---
+++ app/assets/javascripts/components/_body.js.jsx
@@ -0,0 +1,66 @@
+var Body = React.createClass({
+  getInitialState() {
+    return { skills: [] };
+  },
+
+  componentDidMount() {
+    $.getJSON('/api/v1/skills.json', (response) => { this.setState({ skills: response }) });
+  },
+
+  handleSubmit(skill) {
+    var newState = this.state.skills.concat(skill);
+    this.setState({ skills: newState });
+  },
+
+  handleDelete(id) {
+    $.ajax({
+      url: `/api/v1/skills/${id}`,
+      type: 'DELETE',
+      success: () => {
+        this.removeSkillFromDOM(id);
+      }
+    });
+  },
+
+  handleUpdate(skill) {
+    $.ajax({
+      url: `/api/v1/skills/${skill.id}`,
+      type: 'PUT',
+      data: { skill: skill },
+      success: (skill) => {
+        this.updateSkills(skill)
+      }
+    });
+  },
+
+  updateSkills(skill) {
+    var skills = this.state.skills.filter((s) => { return s.id != skill.id });
+    skills.push(skill);
+
+    this.setState({ skills: skills });
+  },
+
+
+  removeSkillFromDOM(id) {
+    var newSkills = this.state.skills.filter((skill) => {
+      return skill.id != id;
+    });
+
+    this.setState({ skills: newSkills });
+  },
+
+  render() {
+    return (
+      <div>
+        <div>
+          <NewSkill handleSubmit={this.handleSubmit}/>
+        </div>
+        <div>
+          <AllSkills skills={this.state.skills}
+                     handleDelete={this.handleDelete}
+                     handleUpdate={this.handleUpdate}/>
+        </div>
+      </div>
+    );
+  }
+});
---
+++ app/assets/javascripts/components/_header.js.jsx
@@ -0,0 +1,7 @@
+var Header = React.createClass({
+  render() {
+    return (
+      <h1>This is my header!</h1>
+    );
+  }
+});
